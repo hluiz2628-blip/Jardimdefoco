0c3bff8bc2c829549b359175f1773481c9b8c0b4
hluiz2628-blip <hluiz2628@gmail.com>
2025-12-27 16:41:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'garden-focus-v2.6'; // Sempre mude isso ao subir nova versão
+const CACHE_NAME = 'garden-focus-v2.6.1'; // Sempre mude isso ao subir nova versão
 
 self.addEventListener('install', () => {
   self.skipWaiting(); // força ativação imediata
